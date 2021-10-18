5969d8dc86b7e3cda731e3d0a2af83467f0952be
Eva <evafhaugum@hotmail.com>
2021-10-18 15:15:40 +0200

    navbar

--- js/header.js
+++ js/header.js
@@ -1,3 +1,27 @@
+/* Navigasjon */
+const myMenu = document.getElementById('menu');
+
+myMenu.onclick=e=>  // JS event delegation
+  {
+  if (e.target.tagName.toLowerCase() != 'a' ) return; // only
+  e.preventDefault;
+  let LI = e.target.parentElement;
+
+  if (!LI.classList.contains('active'))
+    {
+    myMenu.querySelector('li.active').classList.remove('active');
+    }
+  LI.classList.add('active');
+  }
+
+
+var da = new Date();
+document.getElementById("display").innerHTML = da.toDateString();
+
+/* Navigasjon slutt */
+
+
+
 const header = document.getElementById('header');
 
 function headerInsert(){
