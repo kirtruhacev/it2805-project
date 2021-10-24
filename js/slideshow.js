f1ff7a62c83e7206597802b55e48fda430adc9fc
kirtruhacev <kirtruhachev@gmail.com>
2021-10-24 15:22:27 +0200

    h

---
+++ js/slideshow.js
@@ -0,0 +1,16 @@
+var imgList = ["./mage/bilde14.png","bilde15.png","bilde10.png","bilde13.png"]
+var time = 1000;
+var index = 0;
+slideshow();
+
+function slideshow() {
+
+  var x = document.getElementsByClassName("slideshow-img");
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  index++;
+  if (index > x.length) {index = 1}
+  x[index-1].style.display = "block";
+  setTimeout(slideshow, 2000); // Change image every 2 seconds
+}
