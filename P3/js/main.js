48380c425cd5f0f36cfdc54d634813d8d244f561
Eva <evafhaugum@hotmail.com>
2021-10-25 00:53:48 +0200

    p3

---
+++ P3/js/main.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {makeSticky()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function makeSticky() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
