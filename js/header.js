156470ce89d4e4e21217762c0976c741b6b51dc5
Eva <evafhaugum@hotmail.com>
2021-10-24 15:16:04 +0200

    liten header fix, fjerna ekstra loading på kontakt

--- js/header.js
+++ js/header.js
@@ -25,30 +25,35 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) { /* effekten skjer når man scroller over 80 */
-    document.getElementById("header").style.padding = "5px 40px"; /* padding rundt header etter man scroller */
-    document.getElementById("logo").style.height = "50px"; /* størrelse på logo etter man scroller */
+    document.getElementById("header").style.padding = "5px 200px"; /* padding rundt header etter man scroller */
+    document.getElementById("header").style.fontSize = "15px"; /* størrelse på logo etter man scroller */
     document.getElementById("header").style.fontWeight = "normal"; /* endrer font type etter man scroller */
     document.getElementById("header").style.backgroundColor = "black"; /* farge på header etter man scroller */
-    document.getElementById("header").style.height = "60px" /*endrer høyden på headeren ved scrolling */
-    document.getElementById("knappHome").style.fontSize = "15px" /*endrer størrelsen på menyknappene i headeren */
-    document.getElementById("knappMeny").style.fontSize = "15px" /*endrer størrelsen på menyknappene i headeren */
-    document.getElementById("knappGallery").style.fontSize = "15px" /*endrer størrelsen på menyknappene i headeren */
-    document.getElementById("knappOmoss").style.fontSize = "15px" /*endrer størrelsen på menyknappene i headeren */
-    document.getElementById("knappKontakt").style.fontSize = "15px" /*endrer størrelsen på menyknappene i headeren */
-    document.getElementById("header").style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(119, 97, 25, 0.904)"
+    document.getElementById("logo").style.height = "50px"; /* størrelse på logo etter man scroller */
+
+    /* Fjern dette? Ordnet med fontsize som gjelder alle */
+    /*document.getElementById("header").style.height = "60px" /*endrer høyden på headeren ved scrolling */
+    /*document.getElementById("knappHome").style.fontSize = "15px" /*endrer størrelsen på menyknappene i headeren */
+    /*document.getElementById("knappMeny").style.fontSize = "15px" /*endrer størrelsen på menyknappene i headeren */
+    /*document.getElementById("knappGallery").style.fontSize = "15px" /*endrer størrelsen på menyknappene i headeren */
+    /*document.getElementById("knappOmoss").style.fontSize = "15px" /*endrer størrelsen på menyknappene i headeren */
+    /*document.getElementById("knappKontakt").style.fontSize = "15px" /*endrer størrelsen på menyknappene i headeren */
+    /*document.getElementById("header").style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(119, 97, 25, 0.904)"*/
     
   } else {
     document.getElementById("header").style.padding = "50px 10px";
-    /*document.getElementById("header").style.fontSize = "30px";*/
+    document.getElementById("header").style.fontSize = "20px"; /*tilbakestiller størrelse på menyknappene når øverst på side */
     document.getElementById("header").style.fontWeight = "bold";
     document.getElementById("header").style.backgroundColor = "transparent";
     document.getElementById("logo").style.height = "150px" /*tilbakestiller logo når øverst på side */
-    document.getElementById("knappHome").style.fontSize = "20px" /*tilbakestiller størrelse på menyknappene når øverst på side */
-    document.getElementById("knappMeny").style.fontSize = "20px" /*tilbakestiller størrelse på menyknappene når øverst på side */ 
-    document.getElementById("knappGallery").style.fontSize = "20px" /*tilbakestiller størrelse på menyknappene når øverst på side */
-    document.getElementById("knappOmoss").style.fontSize = "20px" /*tilbakestiller størrelse på menyknappene når øverst på side */
-    document.getElementById("knappKontakt").style.fontSize = "20px" /*tilbakestiller størrelse på menyknappene når øverst på side */
-    document.getElementById("header").style.boxShadow = "none"
+
+    /* Fjern dette? Ordnet med fontsize som gjelder alle */
+    /*document.getElementById("knappHome").style.fontSize = "20px" /*tilbakestiller størrelse på menyknappene når øverst på side */
+    /*document.getElementById("knappMeny").style.fontSize = "20px" /*tilbakestiller størrelse på menyknappene når øverst på side */ 
+    /*document.getElementById("knappGallery").style.fontSize = "20px" /*tilbakestiller størrelse på menyknappene når øverst på side */
+    /*document.getElementById("knappOmoss").style.fontSize = "20px" /*tilbakestiller størrelse på menyknappene når øverst på side */
+    /*document.getElementById("knappKontakt").style.fontSize = "20px" /*tilbakestiller størrelse på menyknappene når øverst på side */
+    /*document.getElementById("header").style.boxShadow = "none" */
   }
 }
 
