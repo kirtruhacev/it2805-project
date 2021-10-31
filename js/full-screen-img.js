dabefe98095b211ac7a7aafe92a6b6e68f85f915
kirtruhacev <kirtruhachev@gmail.com>
2021-10-31 13:16:15 +0100

    h

---
+++ js/full-screen-img.js
@@ -0,0 +1,10 @@
+function getPics() {} //just for this demo
+const imgs = document.querySelectorAll("#grid-container-menu img");
+const fullPage = document.querySelector('#fullpage');
+
+imgs.forEach(img => {
+  img.addEventListener('click', function() {
+    fullPage.style.backgroundImage = 'url(' + img.src + ')';
+    fullPage.style.display = 'block';
+  });
+});
