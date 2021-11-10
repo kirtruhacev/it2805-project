2f614dcd159554f34c6ce27a2a62e61bb054935b
kirtruhacev <kirtruhachev@gmail.com>
2021-11-10 21:13:02 +0100

    h

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,10 +1,10 @@
-/* var imgList = ["./mage/bilde14.png","bilde15.png","bilde10.png","bilde13.png"]
-var time = 1000; */
+
 var index = 0;
 slideshow();
 
 function slideshow() {
-
+  /*Funksjonen som aksesserer bilder som hører til slideshowen, og så itererer gjennom
+  listen og endrer display fra none til default verdien, som gir slideshow effekten.*/
   var x = document.getElementsByClassName("slideshow-img");
   for (var i = 0; i < x.length; i++) {
     x[i].style.display = "none";
@@ -12,5 +12,5 @@ function slideshow() {
   index++;
   if (index > x.length) {index = 1}
   x[index-1].style.display = "block";
-  setTimeout(slideshow, 2000); // Change image every 2 seconds
+  setTimeout(slideshow, 2000); // Rullerer bilde hver annen sekund
 }
