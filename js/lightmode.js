dd806632b9d299d8c1caa02b68c439b41a57dfc5
Eva <evafhaugum@hotmail.com>
2021-11-10 15:34:59 +0100

    Update lightmode.js

--- js/lightmode.js
+++ js/lightmode.js
@@ -3,14 +3,14 @@
 let darkMode = true;
 
 function darkmode() {
-    const wasDarkmode = darkMode; // localStorage.getItem('darkmode') === 'true';
-    darkMode = !darkMode; // localStorage.setItem('darkmode', !wasDarkmode);
+    const wasDarkmode = darkMode; 
+    darkMode = !darkMode; 
     const element = document.body;
     element.classList.toggle('light-mode', wasDarkmode);
   }
 
   function onload() {
-    document.body.classList.toggle('light-mode', !darkMode); // localStorage.getItem('darkmode') === 'true');
+    document.body.classList.toggle('light-mode', !darkMode); 
   }
 
 /* Inspirasjon: https://codepen.io/mattmarquise/details/MWbrNWe */
